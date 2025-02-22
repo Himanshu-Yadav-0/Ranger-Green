d1708eb2cca149db05bd39ae925e6cc551c8b5f9
Himanshu-Yadav-0 <himanshuydv0004@gmail.com>
2025-02-22 12:32:28 +0530

    Loads the Plant data file and function to upload data to MongoDB

--- Backend/backend_server.py
+++ Backend/backend_server.py
@@ -18,6 +18,15 @@ plants_collection = db["plants"]
 ESP_SERVER_URL = "http://192.168.0.18.5001/sensor_data"
 UPDATE_THRESHOLD_MOISTURE_URL = "http://192.168.0.18:5001/update_moisture"
 
+#Loads JSON data from the json file as file
+with open("plants_data2.json","r",encoding="utf-8")as file:
+    plants_data = json.load(file)
+
+# Adding dataset to the MongoDb Databse Here: plant_databse
+def add_plant_data():
+    if plants_collection.count_documents({})==0:
+        plants_collection.insert_many(plants_data)
+        print("Plant Dataset Added in the Databse")
 
 if __name__ == '__main__':
     app.run(debug=True)
