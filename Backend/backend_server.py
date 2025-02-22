9fb217996e7cdd7feaddae2f6eeea788afa8adcd
Himanshu-Yadav-0 <himanshuydv0004@gmail.com>
2025-02-22 12:16:32 +0530

    Add API URL for ESP Server and THRESHOLD Moisture

--- Backend/backend_server.py
+++ Backend/backend_server.py
@@ -7,7 +7,16 @@ import json
 app = Flask(__name__)
 CORS(app)
 
+# Setting up Connection to MongoDb Database for the Plant dataset
+MONGO_URL = "mongodb://localhost:27017"
+client = MongoClient(MONGO_URL)
+db = client["plant_database"]
+plants_collection = db["plants"]
 
+
+#Setting up the API URL to ESP SERVER AND THRESHOLD MOISTURE UPDATE
+ESP_SERVER_URL = "http://192.168.0.18.5001/sensor_data"
+UPDATE_THRESHOLD_MOISTURE_URL = "http://192.168.0.18:5001/update_moisture"
 
 
 if __name__ == '__main__':
